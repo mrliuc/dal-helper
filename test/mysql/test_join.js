dd7acca2324304edc457f44cef2e2374920d5c3d
mrliuc <mrliuc@qq.com>
2020-11-23 16:58:18 +0800

    20201123 增加leftJoins

--- test/mysql/test_join.js
+++ test/mysql/test_join.js
@@ -25,3 +25,26 @@ describe('mysql sql', () => {
         return Promise.resolve();
     });
 });
+
+describe('mysql sql', () => {
+    it('select ors', async () => {
+        const options = await dalHelper.select({
+            table: 'TestTable a',
+            columns: ['a.*'],
+            leftJoins: [{
+                table: 'TestTable b',
+                on: 'a.Id=b.Id',
+            }, {
+                table: 'TestTable c',
+                on: 'a.Id=c.Id',
+            }],
+            whereAnd: [['a.Id', 1], ['a.Id', 4]],
+            // ors: [{ Id: 2 }, { Id: 3 }],
+        }).options;
+
+        // console.log(options);
+        assert.equal(' select a.* from TestTable a left join TestTable b on a.Id=b.Id  left join TestTable c on a.Id=c.Id  where (a.Id = :wa_Id00_End and a.Id = :wa_Id01_End);', options[0].sql);
+
+        return Promise.resolve();
+    });
+});
